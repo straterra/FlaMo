5168f37747588850ed8e84f799d5d92abbd06a6f
Thomas A. York <tyork@salesforce.com>
2018-01-31 20:54:56 -0500

    Moved files from DreamerFlask Updated add-user.py for python 3

---
+++ app/tabledef.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# https://pythonspot.com/login-authentication-with-flask/
+
+# Imports
+from sqlalchemy import *
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+from app import *
+
+## Variables
+# DB Name
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], echo=True)
+Base = declarative_base()
+
+
+########################################################################
+
+
+class User(Base):
+    """"""
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    password = Column(String)
+
+    # ----------------------------------------------------------------------
+    def __init__(self, username, password):
+        """"""
+        self.username = username
+        self.password = password
+
+
+# Create tables
+Base.metadata.create_all(engine)
---
+++ db/add-user.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# Written by Thomas York
+
+# Imports
+
+import datetime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from flask_hashing import Hashing
+from app.tabledef import *
+from app import app
+import os
+import getpass
+
+# Setup
+basedir = os.path.abspath(os.path.dirname(__file__))
+engine = create_engine('sqlite:///' + os.path.join(basedir, 'auth.sqlite3'), echo=True)
+
+# Ask user for information
+user = input("Username:")
+passwd = getpass.getpass("Password for " + user + ":")
+
+h = hashing.hash_value(passwd, salt=app.config['SECRET_KEY'])
+
+# create a Session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+user = User(user, h)
+session.add(user)
+
+# commit the record the database
+session.commit()
+
+session.commit()
